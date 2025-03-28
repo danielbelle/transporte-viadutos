4afa273007c2c1337c25a44456608fdc3c92b333
danielbelle <henrique.danielb@gmail.com>
2025-03-28 14:11:32 -0300

    0.0.1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view('contact');
 });
+
+Route::get('/contact', [ContactController::class, 'show'])->name('contact.show');
