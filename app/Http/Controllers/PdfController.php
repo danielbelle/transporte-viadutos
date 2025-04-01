7cec8d3b2e991bfb1139bbd050663bea1326947c
danielbelle <henrique.danielb@gmail.com>
2025-04-01 15:07:31 -0300

    004.2 - create functions to map pdf and simplify text add, and encode ISO-8859-1

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,20 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use setasign\Fpdi\Fpdi;
 
 
 class PdfController extends Controller
 {
-    public $pdfEditor;
-    public function __construct()
-    {
-        $this->pdfEditor = $this->editPdf();
-    }
 
     public function show()
     {
+        $this->editPdf();
         return view('pdfs/pdf');
     }
 
@@ -23,7 +18,6 @@ class PdfController extends Controller
     {
         // Initialize FPDI
         $pdf = new Fpdi();
-
         // Path to existing PDF
         $templatePath = storage_path('app\public\attachments\auxilio-transporte.pdf');
 
@@ -36,20 +30,75 @@ class PdfController extends Controller
         $pdf->useTemplate($templateId);
 
         // Set font and add new text
-        $pdf->SetFont('Helvetica');
-        $pdf->SetFontSize('11');
+        $pdf->SetFont('Helvetica', 'B', 12);
         $pdf->SetTextColor(0, 0, 0);
-        $pdf->SetXY(36, 47); // Position (x,y in mm)
-        $pdf->Write(0, 'Daniel Henrique Belle');
+        $pdf->SetCreator('Daniel Henrique Belle', true);
 
-        $pdf->SetXY(88, 57); // Position (x,y in mm)
-        $pdf->Write(0, '024.449.020-17');
-
-
+        $this->setText($pdf);
         // Output the modified PDF
         $outputPath = storage_path('app/public/attachments/modified.pdf');
         $pdf->Output($outputPath, 'F');
 
         return response()->download($outputPath);
     }
+
+    private function convertIso($string): string
+    {
+        return iconv(mb_detect_encoding($string, mb_detect_order(), true), "ISO-8859-1", $string);
+    }
+
+    private function setText($pdf)
+    {
+        foreach ($this->positions as $key => $position) {
+            $pdf->SetXY($position[0], $position[1]); // Position (x,y in mm)
+            $pdf->Write(0, $this->convertIso($this->formInput[$key]));
+        }
+    }
+
+    private array $positions = [
+        'nameFull' => [37, 46],
+        'docRG' => [88, 56],
+        'docCPF' => [35, 66],
+        'course' => [30, 75],
+        'period' => [77, 85],
+        'institution' => [31, 95],
+        'month' => [101, 124],
+        'timesInMonth' => [170, 124],
+        'city' => [125, 134],
+        'signature' => [86, 160],
+        'signatureName' => [91, 170],
+    ];
+
+    private array $formInput = [
+        'nameFull' => 'Daniel Henrique Bellé',
+        'docRG' => '8097098522',
+        'docCPF' => '024.449.020-17',
+        'period' => '1°',
+        'institution' => 'IFRS',
+        'course' => 'Análise e Desenvolvimento de Sistemas',
+        'month' => 'abril',
+        'timesInMonth' => 20,
+        'city' => 'Erechim',
+        'signature' => 'ASSINATURA',
+        'signatureName' => 'Daniel Henrique Bellé',
+
+    ];
 }
+
+/*
+    private function formInputData($mapDataObject): object
+    {
+        return (object) [
+            'nameFull' => $mapDataObject->nameFull,
+            'docRG' => $mapDataObject->docRG,
+            'docCPF' => $mapDataObject->docCPF,
+            'period' => $mapDataObject->period,
+            'course' => $mapDataObject->course,
+            'month' => $mapDataObject->month,
+            'timesInMonth' => $mapDataObject->timesInMonth,
+            'city' => $mapDataObject->city,
+            'signature' => $mapDataObject->signature,
+        ];
+    }
+
+*/
