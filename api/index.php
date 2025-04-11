aa9817828b9be886c7a6e245c5195bae216e84d3
danielbelle <henrique.danielb@gmail.com>
2025-04-11 17:31:04 -0300

    010.3 - vercel new configs

--- api/index.php
+++ api/index.php
@@ -1,13 +1,15 @@
+
 <?php
 
-// Load the Laravel application
-require __DIR__ . '/../vendor/autoload.php';
+// load the laravel applications
+
+require __DIR__ . '/../bootstrap/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
 $response->send();
 $kernel->terminate($request, $response);
